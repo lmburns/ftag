fb361ce79650423b0c7751454d700ba1df40eb52
Lucas Burns <burnsac@me.com>
2021-07-04 23:44:47 -0500

    updated zsh plugin, better wrapping control, adding everything into one array

--- ftag.plugin.zsh
+++ ftag.plugin.zsh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
-fpath=( "${0:h}/ftag" "${fpath[@]}" )
-autoload -Uz ftag
+typeset -g FTAG_VERSION=0.4
+
+[[ -z ${(M)zsh_loaded_plugins:#lmburns/ftag} && -z ${fpath[(r)${0:h}*]} ]] && {
+  fpath=( "${0:h}/ftag" "${fpath[@]}" )
+  autoload -Uz ftag
+}
